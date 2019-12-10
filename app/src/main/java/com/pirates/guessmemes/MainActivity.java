195086fed311d9565b758312206106b68c074784
Rayun Mehrab <mehrabrym@gmail.com>
2019-12-09 23:37:36 -0500

    Adding welcome message and basic imageview for a quiz.

--- app/src/main/java/com/pirates/guessmemes/MainActivity.java
+++ app/src/main/java/com/pirates/guessmemes/MainActivity.java
@@ -1,13 +1,28 @@
 package com.pirates.guessmemes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.button);
+
+        // Set OnClickListener event on the Button
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
