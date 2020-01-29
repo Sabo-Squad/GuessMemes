b1526bc352d88159aec011b83b9f88a3df4aff1c
Rayun Mehrab <mehrabrym@gmail.com>
2020-01-28 22:24:02 -0500

    Setting up AndroidX and connecting to Google Play Game services

--- app/src/main/java/com/pirates/guessmemes/MainActivity.java
+++ app/src/main/java/com/pirates/guessmemes/MainActivity.java
@@ -1,11 +1,10 @@
 package com.pirates.guessmemes;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button startButton;
--- app/src/main/java/com/pirates/guessmemes/QuizActivity.java
+++ app/src/main/java/com/pirates/guessmemes/QuizActivity.java
@@ -1,6 +1,6 @@
 package com.pirates.guessmemes;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
